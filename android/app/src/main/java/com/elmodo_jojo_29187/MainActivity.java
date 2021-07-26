b50bde326e2c9056a1c61999cccef65d67c96cc9
Crowdbotics <team@crowdbotics.com>
2021-07-26 13:11:37 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/elmodo_jojo_29187/MainActivity.java
@@ -0,0 +1,15 @@
+package com.elmodo_jojo_29187;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "elmodo_jojo_29187";
+  }
+}
